7e9d622136063b3ad1b75ad6b65f1e609b769001
krisnabimantoro <krisnabmntr@gmail.com>
2024-02-13 23:08:04 +0700

    updated

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const layout = () => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient"></div>
+            </div>
+        </body>
+    </html>
+  );
+};
+
+export default layout;
